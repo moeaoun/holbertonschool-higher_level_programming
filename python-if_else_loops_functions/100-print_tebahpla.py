9b74f7b53c176d728b378c6ec51dbe37ab8f0766
root <moe_aoun@hotmail.com>
2025-03-23 12:28:33 +0000

    100-print_tebahpla.py

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def print_tebahpla():
-    print(''.join([chr(122 - i) if i % 2 == 0 else chr(122 - i).upper() for i in range(26)]), end="")
+    print("".join([chr(122 - i) if i % 2 == 0 else chr(122 - i).upper() for i in range(26)]), end="")
 
