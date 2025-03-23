80b1f3be11753c61512abd2a5c3ec2cce84d48b0
root <moe_aoun@hotmail.com>
2025-03-23 12:50:33 +0000

    0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,36 +1,6 @@
 #!/usr/bin/python3
-
-# Open the file and read its contents
-file_content = '''#!/usr/bin/python3
-from add_0 import add
-if __name__ == "__main__":
+if __name == "__main__":
+    from add_0 import add 
     a = 1
-    b = 2
-    print(f"{a} + {b} = {add(a, b)}")
-    a = 10
-    b = 30
-    print(f"{a} + {b} = {add(a, b)}")
-    a = -10
-    b = 30
-    print(f"{a} + {b} = {add(a, b)}")
-    a = -10
-    b = -30
-    print(f"{a} + {b} = {add(a, b)}")
-    a = 5
-    b = "H"
-    print(f"{a} + {b} = {add(a, b)}")'''
-
-# Count occurrences
-number_of_add_0 = file_content.count("add_0")
-number_of_print = file_content.count("print")
-number_of_a = file_content.count("a =")
-number_of_b = file_content.count("b =")
-number_of_add = file_content.count("add(a, b)")
-
-# Print results
-print(f"Number of occurrences of 'add_0': {number_of_add_0}")
-print(f"Number of occurrences of 'print': {number_of_print}")
-print(f"Number of occurrences of 'a =': {number_of_a}")
-print(f"Number of occurrences of 'b =': {number_of_b}")
-print(f"Number of occurrences of 'add(a, b)': {number_of_add}")
-
+    b = 2 
+    print("() + () = ()".format(a, b, add(a, b)))
