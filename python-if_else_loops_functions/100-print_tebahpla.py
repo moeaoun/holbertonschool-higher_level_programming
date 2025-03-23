8330593c015ff5caad01a3973ef8ed302ae9017b
root <moe_aoun@hotmail.com>
2025-03-23 12:27:54 +0000

    100-print_tebahpla.py

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 
 def print_tebahpla():
-    for i in range(25, -1, -1):
-        # Check if i is even (z, x, v,... should be lowercase)
-        if i % 2 == 0:
-            print(chr(122 - i), end="")
-        else:
-            print(chr(122 - i).upper(), end="")
+    print(''.join([chr(122 - i) if i % 2 == 0 else chr(122 - i).upper() for i in range(26)]), end="")
 
