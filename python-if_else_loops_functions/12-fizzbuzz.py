12d292b0aa911beddf37b17a74ed27b352e2a83e
root <moe_aoun@hotmail.com>
2025-03-23 12:23:20 +0000

    12-fizzbuzz.py

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -11,5 +11,4 @@ def fizzbuzz():
         else:
             print(i, end=" ")
     print()  # Print a newline at the end
-                                               
-~                                                      
+        
