aa11e65ff5db35fa8c19625c916e967ace9f0f5d
root <moe_aoun@hotmail.com>
2025-03-23 10:22:02 +0000

    8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-str1 = "object"
-str2 = "-oriented"
-str3 = " programming with Python"
-print(f"{str1}{str2}{str3}")
+print("object" + "-oriented" + " programming with Python")
 
