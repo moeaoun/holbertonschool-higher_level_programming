73c8531cf985dab694b6eb3763a3183031642f68
root <moe_aoun@hotmail.com>
2025-03-23 12:25:59 +0000

    12-fizzbuzz.py

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
@@ -8,4 +10,4 @@ def fizzbuzz():
             print("Buzz", end=" ")
         else:
             print(i, end=" ")
-      
+     
