6d05f8f963f20956b6f1a75a4c73b3437aa2de1f
root <moe_aoun@hotmail.com>
2025-03-27 17:21:04 +0000

    11-delete_at.py

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,8 +1,9 @@
+#!/usr/bin/python3
+
 def delete_at(my_list=[], idx=0):
-    # Check if the index is valid (within the list's range)
     if idx < 0 or idx >= len(my_list):
         return my_list
     
-    # If index is valid, delete the element at that position
-    return my_list[:idx] + my_list[idx+1:]
+    # Create a new list without the item at idx
+    return my_list[:idx] + my_list[idx + 1:]
 
