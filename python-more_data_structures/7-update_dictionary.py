fdc1d5ac8b1eb17a3eeb0975e558257428cb12bb
root <moe_aoun@hotmail.com>
2025-03-27 20:49:31 +0000

    7-update_dictionary.py

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    # Update the dictionary with the new key-value pair (replace or add)
+    a_dictionary[key] = value
+    return a_dictionary
+
