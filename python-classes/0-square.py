edcf5e6b1d0fec32ef259352339fa3eb931583c8
root <moe_aoun@hotmail.com>
2025-03-31 16:55:58 +0000

    0-square.py

---
+++ python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module defines a class Square.
+"""
+
+class Square:
+    """
+    This class defines a square. The class does not have any properties or methods
+    but serves as a placeholder for future functionality related to a square.
+    """
+    pass
+
