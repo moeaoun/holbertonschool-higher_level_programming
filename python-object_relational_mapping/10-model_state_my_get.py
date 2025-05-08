6977eb67dd17e6ad3f5e6105caf63c11f4694ddb
root <moe_aoun@hotmail.com>
2025-05-08 17:22:03 +0000

    10-model_state_my_get.py

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,33 +1,20 @@
 #!/usr/bin/python3
-"""Prints the State object with the name passed as argument from the database."""
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+""" prints the State object with the name passed as argument from the database
+"""
 import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
-    # Parse command-line arguments
-    username, password, db_name, state_name = sys.argv[1:5]
-
-    # Create SQLAlchemy engine
-    engine = create_engine(
-        f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}',
-        pool_pre_ping=True
-    )
-
-    # Bind the engine to a session
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    # Query the database for the state using parameter binding (SQL injection safe)
-    result = session.query(State).filter(State.name == state_name).first()
-
-    # Output result
-    if result:
-        print(result.id)
-    else:
+    instance = session.query(State).filter(State.name == (sys.argv[4],))
+    try:
+        print(instance[0].id)
+    except IndexError:
         print("Not found")
-
-    session.close()
-
