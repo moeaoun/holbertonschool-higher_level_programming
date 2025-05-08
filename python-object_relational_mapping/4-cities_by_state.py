29145aee9d2b7556e4435ec1618968dd76e46792
root <moe_aoun@hotmail.com>
2025-05-08 14:36:17 +0000

    4-cities_by_state.py

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,24 +1,38 @@
 #!/usr/bin/python3
 """
-Script that lists all cities from the database hbtn_0e_4_usa
+Lists all cities from the database hbtn_0e_4_usa with their state names.
+Usage: ./4-cities_by_state.py <mysql_username> <mysql_password> <database_name>
 """
+
 import MySQLdb
-from sys import argv
+import sys
 
-# The code should not be executed when imported
-if __name__ == '__main__':
-    # make a connection to the database
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
+if __name__ == "__main__":
+    # Command-line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
 
+    # Connect to MySQL database
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=password, db=db_name)
+
+    # Create cursor
     cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
-                INNER JOIN states ON cities.state_id = states.id\
-                ORDER BY cities.id ASC")
 
-    rows = cur.fetchall()
-    for i in rows:
-        print(i)
-    # Clean up process
+    # SQL query to get cities and their corresponding states
+    query = """
+        SELECT cities.id, cities.name, states.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        ORDER BY cities.id ASC
+    """
+    cur.execute(query)
+
+    # Print each result
+    for row in cur.fetchall():
+        print(row)
+
+    # Close cursor and connection
     cur.close()
     db.close()
