ca49c6b808ba5b45977a9951d939716b14b05cc5
root <moe_aoun@hotmail.com>
2025-05-08 14:28:00 +0000

    a

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,34 +1,27 @@
 #!/usr/bin/python3
 """
-Lists all states with a name starting with 'N'
-from the database hbtn_0e_0_usa.
-Usage: ./1-filter_states.py <mysql_username> <mysql_password> <database_name>
+Script that lists all states with a name starting with N (upper N)
+from the database
 """
-
 import MySQLdb
-import sys
+from sys import argv
 
-if __name__ == "__main__":
-    # Get command-line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
+# The code should not be executed when imported
+if __name__ == '__main__':
+    # make a connection to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
 
-    # Connect to the MySQL database
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=username, passwd=password, db=db_name)
-
-    # Create a cursor object to execute SQL queries
+    # It gives us the ability to have multiple seperate working environments
+    # through the same connection to the database.
     cur = db.cursor()
 
-    # Execute query to select states starting with 'N'
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name\
+                LIKE BINARY 'N%' ORDER BY id ASC")
 
-    # Fetch and print all matching rows
-    for row in cur.fetchall():
-        print(row)
-
-    # Close the cursor and the database connection
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+    # Clean up process
     cur.close()
     db.close()
-
