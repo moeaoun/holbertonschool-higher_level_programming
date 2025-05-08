8a936c84aa5100c876448485fd5b99848c3e0640
root <moe_aoun@hotmail.com>
2025-05-08 14:39:37 +0000

    2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,35 +1,38 @@
 #!/usr/bin/python3
 """
-Displays all values in the states table where name matches the argument.
-Usage: ./2-my_filter_states.py <mysql_username> <mysql_password>
-        <database_name> <state_name>
+Lists all values in the states table of hbtn_0e_0_usa
+where name matches the user input (argument), using format() for the query.
+Usage: ./2-my_filter_states.py <username> <password> <database_name> <state_name>
 """
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Get command-line arguments
+    # Get input arguments
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
     state_name = sys.argv[4]
 
-    # Connect to the MySQL database
+    # Connect to database
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=username, passwd=password, db=db_name)
 
-    # Create a cursor object to execute SQL queries
+    # Create cursor
     cur = db.cursor()
 
-    # Create the query with the user input using string formatting
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
+    # Create query using format (NOTE: vulnerable to SQL injection by design for this task)
+    query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC".format(state_name)
+
+    # Execute query
     cur.execute(query)
 
-    # Fetch and print matching rows
+    # Print results
     for row in cur.fetchall():
         print(row)
 
-    # Close the cursor and database connection
+    # Close connection
     cur.close()
     db.close()
+
