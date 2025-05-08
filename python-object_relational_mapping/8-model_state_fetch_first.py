0832d273977a2889408190888b76f3f5eed990cb
root <moe_aoun@hotmail.com>
2025-05-08 14:45:45 +0000

    a

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,26 +1,36 @@
 #!/usr/bin/python3
 """
-Script that prints the first State object from the database
+Lists the first State object from the database hbtn_0e_6_usa using SQLAlchemy.
+If the table is empty, print 'Nothing'
 """
 
-from model_state import Base, State
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sys import argv
+from model_state import Base, State
 
 if __name__ == "__main__":
-    # create an engine
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    # Get the arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
 
-    Session = sessionmaker()
-    session = Session(bind=engine)
+    # Create an engine and connect to the MySQL database
+    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost/{db_name}')
 
-    Base.metadata.create_all(engine)
-    s_tate = session.query(State).order_by(State.id).first()
+    # Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    if s_tate:
-        print("{}: {}".format(s_tate.id, s_tate.name))
+    # Query the first state, ordered by id
+    first_state = session.query(State).order_by(State.id).first()
+
+    # Check if the state exists and display it
+    if first_state:
+        print(f"{first_state.id}: {first_state.name}")
     else:
         print("Nothing")
+
+    # Close the session
     session.close()
+
