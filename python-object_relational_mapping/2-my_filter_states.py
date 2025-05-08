887f547f09b4945d96c8878b412bda4d832f9843
root <moe_aoun@hotmail.com>
2025-05-08 14:31:01 +0000

    2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,27 +1,35 @@
 #!/usr/bin/python3
 """
-Script that takes in an argument and displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument
+Displays all values in the states table where name matches the user input.
+Usage: ./2-my_filter_states.py <mysql_username> <mysql_password> <database_name> <state_name>
 """
+
 import MySQLdb
-from sys import argv
+import sys
 
-# The code should not be executed when imported
-if __name__ == '__main__':
+if __name__ == "__main__":
+    # Get command-line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
 
-    # make a connection to the database
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
+    # Connect to the MySQL database
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=password, db=db_name)
 
-    # It gives us the ability to have multiple seperate working environments
-    # through the same connection to the database.
+    # Create a cursor object to execute SQL queries
     cur = db.cursor()
-    nmeSr = "SELECT * FROM states WHERE name LIKE BINARY '{}'".format(argv[4])
-    cur.execute(nmeSr)
 
-    rows = cur.fetchall()
-    for i in rows:
-        print(i)
-    # Clean up process
+    # Construct the query using string formatting (prone to SQL injection)
+    query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC".format(state_name)
+    cur.execute(query)
+
+    # Fetch and print all matching rows
+    for row in cur.fetchall():
+        print(row)
+
+    # Close the cursor and the database connection
     cur.close()
     db.close()
+
